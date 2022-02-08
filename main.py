0f4e2f380fb47efc28f6e3a90f496d727c9dd29f
Sla1merAPK <51483550+Sla1mer@users.noreply.github.com>
2022-02-08 19:51:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import sys
 
-from geocoder import get_ll_span, get_coordinates
+from business import find_business
+from distance import lonlat_distance
+from geocoder import get_coordinates
 from mapapi_PG import show_map
 
 
 def main():
     toponym_to_find = " ".join(sys.argv[1:])
 
-    if toponym_to_find:
-        lat, lon = get_coordinates(toponym_to_find)
-        ll_spn = f"ll={lat},{lon}&spn=0.005,0.005"
-        show_map(ll_spn, "map")
+    lat, lon = get_coordinates(toponym_to_find)
+    address_ll = f"{lat},{lon}"
+    span = "0.005,0.005"
 
-        ll, spn = get_ll_span(toponym_to_find)
-        ll_spn = f"ll={ll}&spn={spn}"
-        show_map(ll_spn, "map")
+    # Получаем координаты ближайшей аптеки.
+    organization = find_business(address_ll, span, "аптека")
+    point = organization["geometry"]["coordinates"]
+    org_lat = float(point[0])
+    org_lon = float(point[1])
+    point_param = f"pt={org_lat},{org_lon},pm2dgl"
 
-        point_param = f"pt={ll}"
-        show_map(ll_spn, "map", add_params=point_param)
-    else:
-        print("No data")
+    show_map(f"ll={address_ll}&spn={span}", "map", add_params=point_param)
+
+    # Добавляем на карту точку с исходным адресом.
+    points_param = point_param + f"~{address_ll},pm2rdl"
+
+    show_map("ll={0}&spn={1}".format(address_ll, span), "map", add_params=points_param)
+
+    # Автопозиционирование
+    show_map(map_type="map", add_params=points_param)
+
+    # Сниппет
+    # Название организации.
+    name = organization["properties"]["CompanyMetaData"]["name"]
+    # Адрес организации.
+    address = organization["properties"]["CompanyMetaData"]["address"]
+    # Время работы
+    time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+    # Расстояние 
+    distance = round(lonlat_distance((lon, lat), (org_lon, org_lat)))
+
+    snippet = f"Название:\t{name}\nАдрес:\t{address}\nВремя работы:\t{time}\n" \
+              f"Расстояние:\t{distance}м."
+    print(snippet)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
